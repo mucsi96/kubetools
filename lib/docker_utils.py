27fd4f743b1b78101ee681df32edc7fb106606be
Igor Bari <mucsi96@gmail.com>
2023-03-17 06:09:59 +0000

    fix docker push

--- lib/docker_utils.py
+++ lib/docker_utils.py
@@ -7,7 +7,6 @@ from lib.version_utils import get_version, set_version
 def build_and_push_img(
     *,
     src: Path,
-    docker_context_path: Path,
     tag_prefix: str,
     image_name: str,
     docker_username: str,
@@ -18,18 +17,19 @@ def build_and_push_img(
     changed, version = get_version(src=src, tag_prefix=tag_prefix, ignore=ignore)
 
     if not changed:
-        print(f'No changes detected since {tag_prefix}:{version}')
+        print(f'No changes detected since {tag_prefix}:{version}', flush=True)
         return
 
-    print(f'Changes detected for {tag_prefix}. New version: {version}')
+    print(f'Changes detected for {tag_prefix}. New version: {version}', flush=True)
 
     subprocess.run(['docker', 'login', '--username', docker_username, '--password-stdin'], input=docker_password.encode(), check=True)
 
-    build_command = ['pack', 'build', f'{image_name}:latest', '--path', str(docker_context_path), '--tag', f'{image_name}:{version}']
-    print(' '.join(build_command))
+    build_command = ['pack', 'build', f'{image_name}:latest', '--path', str(src), '--tag', f'{image_name}:{version}']
+    print(' '.join(build_command), flush=True)
+
     subprocess.run(build_command, check=True)
 
-    subprocess.run(['docker', 'push', f'{image_name}:latest', '--all-tags'], check=True)
+    subprocess.run(['docker', 'push', f'{image_name}', '--all-tags'], check=True)
         
     set_version(tag_prefix=tag_prefix, version=version)
-    print(f'Docker image pushed successfully for {tag_prefix}:{version}')
+    print(f'Docker image pushed successfully for {tag_prefix}:{version}', flush=True)
--- scripts/build.py
+++ scripts/build.py
@@ -15,7 +15,6 @@ docker_password = data['docker_password']
 
 build_and_push_img(
     src=root_directory / 'demo_app/client',
-    docker_context_path=root_directory / 'demo_app/client',
     ignore=['node_modules', 'dist'],
     tag_prefix='demo-app-client',
     image_name='mucsi96/kubetools-demo-app-client',
@@ -25,7 +24,6 @@ build_and_push_img(
 
 build_and_push_img(
     src=root_directory / 'demo_app/server',
-    docker_context_path=root_directory / 'demo_app/server',
     tag_prefix='demo-app-server',
     image_name='mucsi96/kubetools-demo-app-server',
     docker_username=docker_username,
@@ -34,7 +32,6 @@ build_and_push_img(
 
 build_and_push_img(
     src=root_directory / 'spring-boot-admin',
-    docker_context_path=root_directory / 'spring-boot-admin',
     tag_prefix='spring-boot-admin',
     image_name='mucsi96/kubetools-spring-boot-admin',
     docker_username=docker_username,
