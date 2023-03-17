93bfde4f202517b071e97ab2cebff07a666752d7
Igor Bari <mucsi96@gmail.com>
2023-03-17 12:04:06 +0000

    fix change depection part 3

--- lib/version_utils.py
+++ lib/version_utils.py
@@ -3,6 +3,7 @@ import subprocess
 import re
 from typing import List
 
+
 def get_previous_tag(tag_prefix):
     [status, prev_tag] = subprocess.getstatusoutput(
         f'git describe --tags --match={tag_prefix}-[1-9]* --abbrev=0')
@@ -16,8 +17,15 @@ def get_previous_tag(tag_prefix):
 def has_source_code_changed(src: Path, prev_tag: str, ignore: List[str]):
     ignore_str = ' '.join(map(lambda x: f'\':!{x}\'', ignore))
     print(f'Detecting changes in {src} since {prev_tag}', flush=True)
-    changed_files = subprocess.run(list(filter(bool, ['git', 'diff', '--name-only', 'HEAD', prev_tag, '--', '.', ignore_str])), cwd=src).stdout        
-    return bool(changed_files)
+    args = list(filter(bool, ['git', 'diff', '--name-only',
+                              'HEAD', prev_tag, '--', '.', ignore_str]))
+    changed_files = subprocess.run(args, cwd=src, capture_output=True)
+    if changed_files.stderr:
+        print(changed_files.stderr.decode())
+    if changed_files.stdout:
+        print(changed_files.stdout.decode())
+    return bool(changed_files.stdout)
+
 
 def get_latest_version(tag_prefix: str):
     [status, tags] = subprocess.getstatusoutput(
@@ -46,6 +54,7 @@ def get_version(src: Path, tag_prefix: str, ignore: List[str]) -> tuple[bool, in
         new_version = 1
 
     return True, new_version
+
 
 def set_version(tag_prefix: str, version: int) -> str:
     subprocess.getstatusoutput(f'git tag "{tag_prefix}-{version}"')
