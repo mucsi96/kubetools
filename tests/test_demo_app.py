d19b60ef0bd5fdd256bdf873be2dd8a1b2126f62
Igor Bari <mucsi96@gmail.com>
2023-03-18 20:14:01 +0000

    replace message

--- tests/test_demo_app.py
+++ tests/test_demo_app.py
@@ -2,4 +2,4 @@ from utils import get_hostname, wait_for_text
 
 def test_demo_app(browser):
     browser.get(f'https://demo.{get_hostname()}')
-    wait_for_text(browser, 'Hello from Ansible Roles Spring Demo!')
+    wait_for_text(browser, 'Hello from kubetools demo!')
