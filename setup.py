fe83c65b38189729be2bd19d3935258ea86bff22
Igor Bari <mucsi96@gmail.com>
2023-03-25 17:54:30 +0000

    add init

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from os import getenv
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name='kubetools',
     version=f'0.{getenv("LIB_VERSION")}.0',
     author="Igor Bari",
-    packages=find_packages(where='lib'),
     package_dir={"": "lib"}
 )
