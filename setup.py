ec1bc9a13d56acab268123d90c54cc5242f03f9c
Igor Bari <mucsi96@gmail.com>
2023-03-25 10:31:48 +0000

    try to fix the wheel

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 
 setup(
     name='kubetools',
-    version=f'0.{getenv("LIB_VERSION")}.0',
+    version=f'0.1.0',
     author="Igor Bari",
-    install_requires=['argon2-cffi'],
     package_dir={"kubetools": "lib"}
 )
