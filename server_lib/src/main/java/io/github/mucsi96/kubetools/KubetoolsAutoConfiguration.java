812d27e9a289f512a33f404e9ee4b55da783a67a
Igor Bari <mucsi96@gmail.com>
2023-03-28 19:40:45 +0000

    Add KubetoolsAutoConfiguration

---
+++ server_lib/src/main/java/io/github/mucsi96/kubetools/KubetoolsAutoConfiguration.java
@@ -0,0 +1,10 @@
+package io.github.mucsi96.kubetools;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class KubetoolsAutoConfiguration {
+    
+}
