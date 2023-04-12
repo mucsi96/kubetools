410d0b5d55da7526150dce8930c1f1f84ad0dc1f
Igor Bari <mucsi96@gmail.com>
2023-04-12 20:15:33 +0000

    fix client image build

--- lib/docker_utils.py
+++ lib/docker_utils.py
@@ -25,9 +25,10 @@ def build_and_push_client_img(
         image_name=image_name,
         pack_args=[
             '--builder',   'paketobuildpacks/builder:base',
-            '--buildpack', 'paketo-buildpacks/nginx',
+            '--buildpack', 'paketo-buildpacks/web-servers',
             '--buildpack', 'paketo-buildpacks/source-removal',
             '--env',       'BP_NODE_RUN_SCRIPTS=build',
+            '--env',       'BP_WEB_SERVER=nginx',
             '--env',       'BP_WEB_SERVER_ROOT=dist',
             '--env',       'BP_INCLUDE_FILES=dist/**',
             *pack_args,
