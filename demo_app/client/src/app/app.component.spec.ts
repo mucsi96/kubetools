57bd29d52872396400fe96b5586a8e1f49780be1
Igor Bari <mucsi96@gmail.com>
2023-11-24 20:44:02 +0000

    fix client tests

--- demo_app/client/src/app/app.component.spec.ts
+++ demo_app/client/src/app/app.component.spec.ts
@@ -1,26 +1,43 @@
 import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterOutlet } from '@angular/router';
 
-@Component({ selector: 'app-message', template: '' })
-class MessageStubComponent {}
+@Component({
+  standalone: true,
+  selector: 'router-outlet',
+  template: '',
+})
+class MockRouterOutlet {}
+
+async function setup() {
+  await TestBed.configureTestingModule({
+    providers: [
+    ],
+  }).compileComponents();
+
+  TestBed.overrideComponent(AppComponent, {
+    remove: {
+      imports: [RouterOutlet],
+    },
+    add: {
+      imports: [MockRouterOutlet],
+    },
+  });
+
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+
+  return {
+    fixture,
+    element: fixture.nativeElement as HTMLElement,
+  };
+}
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent, MessageStubComponent],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should create the app', async () => {
+    const { fixture } = await setup();
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
   });
 });
