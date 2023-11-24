18bb44f1c823229140d6fc7cd11e2e0060837ec9
Igor Bari <mucsi96@gmail.com>
2023-11-24 15:08:14 +0000

    add dockerfile

--- token_handler/src/index.ts
+++ token_handler/src/index.ts
@@ -8,21 +8,36 @@ const PORT = process.env.PORT || 8080;
 const server = http.createServer(
   async (req: IncomingMessage, res: ServerResponse) => {
     try {
+      if (req.url === "/health" && req.method === "GET") {
+        res.writeHead(200);
+        res.end("Health check passed!");
+        return;
+      }
+
       if (req.url === "/authorize" && req.method === "POST") {
-        await authorize(req, res);
+        return await authorize(req, res);
       }
 
       if (req.url === "/get-token" && req.method === "POST") {
-        await getToken(req, res);
+        return await getToken(req, res);
       }
 
       return returnError(res, 404, "Route not found");
     } catch (e) {
-      returnError(res, 500, "Something went wrong");
+      return returnError(res, 500, "Something went wrong");
     }
   }
 );
 
 server.listen(PORT, () => {
   console.log(`server started on port: ${PORT}`);
+});
+
+// Graceful shutdown on SIGTERM signal
+process.on("SIGTERM", () => {
+  console.info("Received SIGTERM signal. Closing server gracefully.");
+  server.close(() => {
+    console.log("Server closed. Exiting process.");
+    process.exit(0);
+  });
 });
